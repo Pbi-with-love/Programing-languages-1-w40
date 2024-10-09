0172136dd14a2dc51e469d388059666a5761be67
Pbi-with-love <anhtai210406@gmail.com>
2024-10-10 03:02:57 +0700

    fix e02

--- e02.java
+++ e02.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 public class e02 {
     public static void main(String[] args) {
         Scanner x = new Scanner(System.in);
-        System.out.println("Enter a name: ");
+        System.out.print("Enter a name: ");
         String s = x.nextLine();
         System.out.println("You entered: " + s);
         for(int i=0; i<s.length();i++){
